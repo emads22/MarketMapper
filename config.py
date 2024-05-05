26ac59930bfd726e9f9b43044f3fe5aa6674d592
emads22 <emadsaab722@gmail.com>
2024-05-05 17:32:50 +0300

    refactor files

--- config.py
+++
@@ -1,7 +0,0 @@
-from pathlib import Path
-
-
-# Specify the path to the ChromeDriver executable
-# Make sure to download the appropriate ChromeDriver version matching existing Chrome version
-CHROMEDRIVER_PATH = Path("./chromedriver-win64") / "chromedriver.exe"
-URL = "http://automated.pythonanywhere.com"
